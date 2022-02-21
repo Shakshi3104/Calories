f1864ec816fe775e85aba65e321f56e44d1ea6f6
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2022-02-21 11:03:36 +0900

    Initial Commit

---
+++ Calories/CaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesApp.swift
+//  Calories
+//
+//  Created by MacBook Pro M1 on 2022/02/21.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calories
+//
+//  Created by MacBook Pro M1 on 2022/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
