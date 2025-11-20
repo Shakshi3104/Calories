d6d805fea47bd0a87b4d610b1a3cec75a2cc8b9a
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2025-11-20 18:40:02 +0900

    refactor: Reorder properties in DailyAdvice struct

--- Calories/Model/FoundationsModels/DailyAdvice.swift
+++ Calories/Model/FoundationsModels/DailyAdvice.swift
@@ -11,10 +11,10 @@ import FoundationModels
 @available(iOS 26.0, *)
 @Generable
 struct DailyAdvice {
+    /// Overall comment
+    var generalComment: String
     /// Advice about dietary
     var dietarySuggestion: String
     /// Advice about exercise
     var exerciseSuggestion: String
-    /// Overall comment
-    var generalComment: String
 }
